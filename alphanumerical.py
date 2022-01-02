5457375330277bf5d00f1b0d0f475f0d65f23ce8
RobbeLauwers <robbe.lauwers@student.uantwerpen.be>
2022-01-02 10:48:03 +0100

    Convert articles to only keep alphanumerical characters. Some articles contained a long series of successive punctuation marks, which might give poor minhash results if only actual text is plagiarised. Using code from https://www.programiz.com/python-programming/csv and https://stackoverflow.com/a/1276774

---
+++ alphanumerical.py
@@ -0,0 +1,15 @@
+import csv
+import re
+
+filenames = [["./data/news_articles_large.csv","./data/news_articles_large_alphanumerical.csv"], ["./data/news_articles_small.csv", "./data/news_articles_small_alphanumerical.csv"]]
+
+for filename in filenames:
+    # https://www.programiz.com/python-programming/csv
+    with open(filename[0], 'r') as input_file:
+        with open(filename[1],"w+",newline="\n") as output_file:
+            reader = csv.reader(input_file)
+            writer = csv.writer(output_file)
+            for row in reader:
+                # https://stackoverflow.com/a/1276774
+                writer.writerow([row[0],re.sub(r'[^a-zA-Z0-9 ]', '', row[1])])
+
