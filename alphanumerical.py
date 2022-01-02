e4d25bb1fda5a37782d171259dfd7a9b0098e56d
RobbeLauwers <robbe.lauwers@student.uantwerpen.be>
2022-01-02 14:34:47 +0100

    Lowercase articles, shingling

--- alphanumerical.py
+++ alphanumerical.py
@@ -1,16 +1,16 @@
 import csv
 import re
 
-filenames = [["./data/news_articles_large.csv","./data/news_articles_large_alphanumerical.csv"], ["./data/news_articles_small.csv", "./data/news_articles_small_alphanumerical.csv"]]
+filenames = [["./data/news_articles_large.csv", "./data/news_articles_large_alphanumerical.csv"],
+             ["./data/news_articles_small.csv", "./data/news_articles_small_alphanumerical.csv"]]
 
 for filename in filenames:
     # https://www.programiz.com/python-programming/csv
     with open(filename[0], 'r') as input_file:
-        with open(filename[1],"w+",newline="\n") as output_file:
+        with open(filename[1], "w+", newline="\n") as output_file:
             reader = csv.reader(input_file)
             writer = csv.writer(output_file)
             for row in reader:
                 # https://stackoverflow.com/a/1276774
                 # https://stackoverflow.com/a/1546244
-                writer.writerow([row[0],  re.sub(' +',' ',re.sub(r'[^a-zA-Z0-9 ]', '', row[1]))  ])
-
+                writer.writerow([row[0], re.sub(' +', ' ', re.sub(r'[^a-zA-Z0-9 ]', '', row[1])).lower()])
---
+++ shingling.py
@@ -0,0 +1,16 @@
+def shingle(article, shingle_length, elements_returned_are_lists=True):
+    article_tokens = article.split(' ')
+    results = []
+    for i in range(len(article_tokens)-(shingle_length-1)):
+        tokens = []
+        for j in range(shingle_length):
+            tokens.append(article_tokens[i+j])
+        if elements_returned_are_lists:
+            results.append(tokens)
+        else:
+            result_str = ""
+            for token in tokens:
+                result_str += (token + "-")
+            result_str = result_str[:-1]
+            results.append(result_str)
+    return results
